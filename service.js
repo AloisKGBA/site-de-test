c10f9aeecbd28db508c31e00c8741338c8e9b6d5
Jolan PILLOT <jpillot@guardiaschool.Fr>
2024-11-21 17:43:58 +0100

    recup main

---
+++ service.js
@@ -0,0 +1,13 @@
+function toggleText(texte) {
+    var moreText = document.getElementById("moreText"+texte);
+    console.log(moreText)
+    var btnText = document.getElementById("viewMoreBtn"+texte);
+
+    if (moreText.style.display === "none") {
+        moreText.style.display = "block";
+        btnText.innerHTML = "Voir moins";
+    } else {
+        moreText.style.display = "none";
+        btnText.innerHTML = "Voir plus";
+    }
+}
