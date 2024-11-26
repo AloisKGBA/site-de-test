fc1c2848ed97f9452512efa042f3b01661eb1697
Jolan PILLOT <jpillot@guardiaschool.Fr>
2024-11-26 10:15:02 +0100

    test php

--- traitement.php
+++ traitement.php
@@ -1,29 +1,47 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-//Connection à la base de donnée de Otawa
-// try {
-//     $pdo = new PDO('mysql:host=nom_hôte;dbname=otawadev', 'utilisateur', 'mot_de_passe');
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// } catch (PDOException $e) {
-//     echo "Erreur de connexion : " . $e->getMessage();
-//     exit();
-// }
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $nom = isset($_GET['nom']) ? htmlspecialchars(trim($_GET['nom'])) : '';
-    $email = isset($_GET['email']) ? htmlspecialchars(trim($_GET['email'])) : '';
-    $message = isset($_GET['message']) ? htmlspecialchars(trim($_GET['message'])) : '';
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'localhost';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'jolan.pillot@gmail.com';                     //SMTP username
+    $mail->Password   = 'Link.1610';                               //SMTP password
+    $mail->SMTPSecure = "ssl";            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    if (!empty($nom) && !empty($email) && !empty($message)) {
-        echo "<h1>Données reçues :</h1>";
-        echo "<p><strong>Nom :</strong> $nom</p>";
-        echo "<p><strong>Email :</strong> $email</p>";
-        echo "<p><strong>Message :</strong> $message</p>";
-    } else {
-        echo "<p style='color: red;'>Veuillez remplir tous les champs du formulaire.</p>";
-    }
-} else {
-    echo "<p style='color: red;'>Le formulaire n'a pas été soumis correctement.</p>";
+    //Recipients
+    $mail->setFrom('jpillot@guardiaschool.fr', 'Mailer');
+    $mail->addAddress('jpillot@guardiaschool.fr', 'Joe User');     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-?>
