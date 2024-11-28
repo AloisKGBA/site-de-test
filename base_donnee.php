ff41d9cea5a5159c524c07661f0cee3ff5e03e4d
Jolan PILLOT <jpillot@guardiaschool.Fr>
2024-11-28 14:55:53 +0100

    mettre sur main

--- base_donnee.php
+++ base_donnee.php
@@ -16,8 +16,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $message_ut = isset($_POST['message']) ? trim($_POST['message']) : '';
 
     if (!empty($nom) && !empty($email) && !empty($message_ut)) {
-        $sql = "SELECT $lien_cv FROM $formulaire_cv";
-        echo "le lien est : $sql";
+        $sql = "INSERT INTO formulaire (nom, Adresse_mail, message) VALUES (:nom, :email, :message_ut)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':message_ut', $message_ut);
+
+        try {
+            $stmt->execute();
+            echo "Données insérées avec succès !";
+        } catch (PDOException $e) {
+            echo "<p style='color: red;'>Erreur lors de l'insertion : " . $e->getMessage() . "</p>";
+        }
     } else {
         echo "<p style='color: red;'>Veuillez remplir tous les champs du formulaire.</p>";
     }
