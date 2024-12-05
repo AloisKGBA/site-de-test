340a45a64b9719a953960ff8f05d6558315f7d11
Poudevigne Evan <epoudevigne@guardiaschool.fr>
2024-12-05 15:16:19 +0100

    push email.php

--- php/email.php
+++ php/email.php
@@ -1,6 +1,6 @@
 <?php
 // Inclure l'autoloader généré par Composer
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
